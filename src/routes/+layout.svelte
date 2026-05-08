fb15acf224073f4225a2eb99877786f7d95d5ba8
PylSerg <pylypenko.sn@gmail.com>
2026-05-08 15:51:36 +0300

    feat: add custom branding with new favicon and manifest icon resources

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
+	import { pwaInfo } from "virtual:pwa-info";
 	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
+	let webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : "";
 </script>
 
 <svelte:head>
@@ -9,4 +11,5 @@
 	<title>Kvoka Board</title>
 </svelte:head>
 
+{@html webManifestLink}
 {@render children()}
