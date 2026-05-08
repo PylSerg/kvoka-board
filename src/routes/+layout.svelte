80e0a82072ef0cd7d8e69853c26724dccc7e29a6
PylSerg <pylypenko.sn@gmail.com>
2026-05-08 16:06:57 +0300

    feat: add 192px icon to manifest and implement service worker registration in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,32 @@
 <script>
 	import { pwaInfo } from "virtual:pwa-info";
+	import { onMount } from "svelte";
 	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
-	let webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : "";
+
+	onMount(async () => {
+		if (pwaInfo) {
+			const { registerSW } = await import("virtual:pwa-register");
+			registerSW({
+				immediate: true,
+				onRegistered(r) {
+					console.log("SW Registered:", r);
+				},
+				onRegisterError(error) {
+					console.log("SW Registration error:", error);
+				},
+			});
+		}
+	});
+
+	let webManifestLink = pwaInfo ? pwaInfo.webManifestLink : "";
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<title>Kvoka Board</title>
+	{@html webManifestLink}
 </svelte:head>
 
-{@html webManifestLink}
 {@render children()}
