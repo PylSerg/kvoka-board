4966b9681967ab27f8e76cba10d514334aa1738a
PylSerg <pylypenko.sn@gmail.com>
2026-05-08 11:20:41 +0300

    Auto deploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Kvoka Board</title>
 </svelte:head>
 
 {@render children()}
